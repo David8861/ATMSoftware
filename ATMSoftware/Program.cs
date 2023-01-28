782ae1dd748aac324c60e6654ab00146b209d5da
Dave <davida8861@gmail.com>
2023-01-28 10:59:57 -0500

    Declared strings and integers for bank account.

--- ATMSoftware/Program.cs
+++ ATMSoftware/Program.cs
@@ -10,12 +10,14 @@ namespace ATMSoftware
     {
         static void Main(string[] args)
         {
-            
+            string accountowner, address;
+            int phonenumber, dob, ssn;
 
             Console.WriteLine("Welcome to Polestar National Bank. I see you're new here. Let's open an account for you...");
             Console.WriteLine("We'll need your name, address, date of birth, phone number, and social security number.");
             Console.WriteLine("What's your name?");
             accountowner = Console.ReadLine();
+
 
         }
     }
