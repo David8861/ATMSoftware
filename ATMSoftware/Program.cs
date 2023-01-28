06873c5f8ba384ca1cd1c012264d4a2177c33514
Dave <davida8861@gmail.com>
2023-01-28 11:12:28 -0500

    Wrote lines for account owner's address, number, and dob.

--- ATMSoftware/Program.cs
+++ ATMSoftware/Program.cs
@@ -15,8 +15,11 @@ namespace ATMSoftware
 
             Console.WriteLine("Welcome to Polestar National Bank. I see you're new here. Let's open an account for you...");
             Console.WriteLine("We'll need your name, address, date of birth, phone number, and social security number.");
-            Console.WriteLine("What's your name?");
-            accountowner = Console.ReadLine();
+            Console.WriteLine("What's your name?"); accountowner = Console.ReadLine();
+            Console.WriteLine("Okay " + accountowner + ". I'll need your address next."); address = Console.ReadLine();
+            Console.WriteLine("Great. Your phone number next..."); phonenumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Last, I'll need your date of birth."); dob = Convert.ToInt32(Console.ReadLine());
+            
 
 
         }
