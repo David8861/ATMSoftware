f121fe04dd2e55b8320c5b7f5dbe5cc296ed7b88
Dave <davida8861@gmail.com>
2023-01-28 10:57:07 -0500

    Add project files.

---
+++ ATMSoftware/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMSoftware
+{
+    class ATMSoftware
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Welcome to Polestar National Bank. I see you're new here. Let's open an account for you...");
+            Console.WriteLine("We'll need your name, address, date of birth, phone number, and social security number.");
+            Console.WriteLine("What's your name?");
+            accountowner = Console.ReadLine();
+
+        }
+    }
+}
