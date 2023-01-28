27426ea58bba05c0c08368c0acb6d32f58283df9
Dave <davida8861@gmail.com>
2023-01-28 12:24:25 -0500

    Changed ssn to long data type. Asked for ssn for user.

--- ATMSoftware/Program.cs
+++ ATMSoftware/Program.cs
@@ -11,11 +11,11 @@ namespace ATMSoftware
         static void Main(string[] args)
         {
             string accountowner, address;
-            int dob, ssn;
-            long telephonenumber;
+            int dob;
+            long telephonenumber, ssn;
 
             Console.WriteLine("Welcome to Polestar National Bank. I see you're new here. Let's open an account for you...");
-            Console.WriteLine("Polestar is dedicated to providing you fast, safe banking.");
+            Console.WriteLine("Polestar is dedicated to providing you fast, secure banking.");
             Console.WriteLine("What's your name?"); accountowner = Console.ReadLine();
             Console.WriteLine("Okay " + accountowner + ". I'll need your address next."); address = Console.ReadLine();
             Console.WriteLine("Great. Your phone number next..."); telephonenumber = Convert.ToInt64(Console.ReadLine());
@@ -30,7 +30,9 @@ namespace ATMSoftware
                 Console.WriteLine("Alright, " + accountowner + ", I'll start you on Polestar's Junior Checking Account.");
                 Console.WriteLine("When you turn 18, you're account will automatically change to our Convenience Account.");
             }
-
+            Console.WriteLine("I'll need your 9-digit social security number in order to activate your account, " + accountowner + ".");
+            Console.WriteLine("Please type it like so: XXX-XX-XXXX");
+            ssn = Convert.ToInt64(Console.ReadLine());
 
 
         }
