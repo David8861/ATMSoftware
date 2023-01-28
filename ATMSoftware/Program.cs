1384dc365480ff9a3cb3ff67a9d437b96ff289b6
Dave <davida8861@gmail.com>
2023-01-28 17:25:44 -0500

    Added account choice string. Rewrote some console lines.

--- ATMSoftware/Program.cs
+++ ATMSoftware/Program.cs
@@ -6,12 +6,12 @@ namespace ATMSoftware
     {
         private static void Main(string[] args)
         {
-            string ownerfirstname, ownerlastname, address, ssn, confirmssn, ssnretry, telephonenumber;
+            string ownerfirstname, ownerlastname, address, ssn, confirmssn, ssnretry, telephonenumber, accountchoice;
             string starterATMcard = "4321 5703 4489 3400";
             int dob, bankpin, bankpinconfirm, banktryagain, pinverification;
             int newaccountgift = 150;
 
-            Console.WriteLine("Welcome to Polestar National Bank. I see you're new here. Let's open an account for you...");
+            Console.WriteLine("Welcome to Polestar National Bank. Let's open an account for you...");
             Console.WriteLine("Polestar is dedicated to providing you fast, secure banking.");
             Console.WriteLine("What's your first name?"); ownerfirstname = Console.ReadLine();
             Console.WriteLine("And your last?"); ownerlastname = Console.ReadLine();
@@ -20,7 +20,7 @@ namespace ATMSoftware
             Console.WriteLine("Last, I'll need your date of birth."); dob = Convert.ToInt32(Console.ReadLine());
             if (dob <= 2004)
             {
-                Console.WriteLine("Alright, " + ownerfirstname + ". I'll start you on Polestar's Convenience Checking Account.");
+                Console.WriteLine("Alright, " + ownerfirstname + ". I'll start you on our Convenience Checking Account.");
             }
             else if (dob >= 2004)
             {
@@ -82,8 +82,8 @@ namespace ATMSoftware
             }
             while (bankpin != bankpinconfirm);
 
-            Console.WriteLine("Before we give you a Polestar ATM card, we would like to gift you $" + newaccountgift + " for opening an account with us today!");
-            Console.WriteLine("Here is your ATM card, " + ownerfirstname + ": " + starterATMcard + ".");
+            Console.WriteLine("Before we give you an ATM card, we would like to gift you $" + newaccountgift + " for opening an account with us!");
+            Console.WriteLine("Your ATM card, " + ownerfirstname + ": " + starterATMcard + ".");
             Console.WriteLine("Account Information for " + ownerfirstname + " " + ownerlastname + ".");
             Console.WriteLine("Address: " + address + "."); Console.Write("Phone Number: " + telephonenumber + ".");
             Console.WriteLine("Date of Birth: " + dob + "."); Console.WriteLine("Social Security Number: " + ssn + ".");
@@ -92,7 +92,7 @@ namespace ATMSoftware
             
             do
             {
-                Console.WriteLine("What is your Pin number?");
+                Console.WriteLine("What is your pin number?");
                 Console.ForegroundColor = ConsoleColor.Black;
                 pinverification = Convert.ToInt32(Console.ReadLine());
                 Console.ForegroundColor = ConsoleColor.White;
@@ -111,6 +111,8 @@ namespace ATMSoftware
 
             Console.WriteLine(ownerfirstname + ", we are here to make banking safer, together.");
             Console.WriteLine("Type \"Check Balance\" to see the funds in your account. Type \"Withdraw\" to withdraw funds. Or type \"Deposit\" to deposit funds.");
+            accountchoice = Console.ReadLine();
+
         }
     }
 }
