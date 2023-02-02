4695f072931d1a7da52c3e516a91ae51834d5c19
Dave <davida8861@gmail.com>
2023-02-02 08:21:21 -0500

    Removed Banking.cs functionality.

--- ATMSoftware/Banking.cs
+++ ATMSoftware/Banking.cs
@@ -38,28 +38,7 @@ namespace ATMSoftware
             Console.WriteLine("We are here to make banking safer, together.");
             Console.WriteLine("Type \"Check Balance\" to see the funds in your account. Type \"Withdraw\" to withdraw funds. Or type \"Deposit\" to deposit funds.");
             accountchoice = Console.ReadLine();
-            do
-            {
-                Console.WriteLine("Your current balance is " + newaccountgift + ".");
-                break;
-            }
-            while (accountchoice == "Check Balance");
-            do
-            {
-                Console.WriteLine("How much would you like to deposit?");
-                depositamount = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Your new balance is: " + (depositamount + newaccountgift));
-                break;
-            }
-            while (accountchoice == "Deposit");
-            do
-            {
-                Console.WriteLine("How much would you like to withdraw?");
-                withdrawamount = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("You withdrew " + withdrawamount + ". Your account currently has " + (newaccountgift - withdrawamount) + ", left.");
-                break;
-            }
-            while (accountchoice == "Withdraw");
+            
         }
     }
 }
